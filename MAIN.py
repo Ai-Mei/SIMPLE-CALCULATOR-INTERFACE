58ca0b3206eee9ff9e0102edbec5498dbd8f5bcd
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-24 11:48:32 +0800

    Added colors

--- MAIN.py
+++ MAIN.py
@@ -3,10 +3,11 @@ from USERINTERFACE import UserInterface
 
 
 root = tk.Tk()
+root.configure(bg="#D9D7F1")
 root.geometry("400x300")
 root.title("My Calculator")
 
-label = tk.Label(root, text="My Calculator", font=("Georgia", 16))
+label = tk.Label(root, text="My Calculator", font=("Georgia", 16), bg="#D9D7F1", fg="#5E454B")
 label.pack(padx=20, pady=10)
 
 UserInterface(root)
--- USERINTERFACE.py
+++ USERINTERFACE.py
@@ -11,25 +11,25 @@ class UserInterface:
     
     def CreateWidgets(self):
         # Ask the user for inputs:
-        label1 = tk.Label(self.root, text="Input the first number:")
+        label1 = tk.Label(self.root, text="Input the first number:", bg="#D9D7F1", fg="#354259")
         label1.pack()
         self.entry1 = tk.Entry(self.root)
         self.entry1.pack()
 
-        label2 = tk.Label(self.root, text="Input the second number:")
+        label2 = tk.Label(self.root, text="Input the second number:", bg="#D9D7F1", fg="#354259")
         label2.pack()
         self.entry2 = tk.Entry(self.root)
         self.entry2.pack()
 
 
         # Ask user for operatuion
-        label3 = tk.Label(self.root, text="Type the operation:")
+        label3 = tk.Label(self.root, text="Type the operation:", bg="#D9D7F1", fg="#AD8B73")
         label3.pack()
         self.entry3 = tk.Entry(self.root)
         self.entry3.pack()
 
         # Enter
-        button = tk.Button(self.root, text="Process Inputs", command=self.execute)
+        button = tk.Button(self.root, text="Process Inputs", bg="#FDEFEF", fg="#B980F0", command=self.execute)
         button.pack(pady=10)
  
 
@@ -39,15 +39,15 @@ class UserInterface:
         part = 0
         while time.time() - start_time < duration:
             if part == 0:
-                loading_label.config(text="Loading")
+                loading_label.config(text="Loading", bg="#D9D7F1", fg="#F2D8D8")
             elif part == 1:
-                loading_label.config(text="⋆.ೃLoading࿔*:･*")
+                loading_label.config(text="⋆.ೃLoading࿔*:･*", bg="#D9D7F1", fg="#5C8984")
             elif part == 2:
-                loading_label.config(text="°❀⋆.ೃLoading࿔*:･*:･⋆")
+                loading_label.config(text="°❀⋆.ೃLoading࿔*:･*:･⋆", bg="#D9D7F1", fg="#545B77")
             elif part == 3:
-                loading_label.config(text=".ೃ°❀⋆.ೃLoading࿔*:･*:･⋆.ೃ")
+                loading_label.config(text=".ೃ°❀⋆.ೃLoading࿔*:･*:･⋆.ೃ", bg="#D9D7F1", fg="#374259")
             elif part == 4:
-                loading_label.config(text="⋆.ೃ°❀⋆.ೃLoading࿔*:･*:･⋆.ೃ࿔*:･")
+                loading_label.config(text="⋆.ೃ°❀⋆.ೃLoading࿔*:･*:･⋆.ೃ࿔*:･", bg="#D9D7F1", fg="#7A3E3E")
             self.root.update()
             time.sleep(0.5)
             part = (part + 1) % 5
@@ -81,10 +81,10 @@ class UserInterface:
         elif operation == "/":
             result = op.Division(num_1, num_2)
         else:
-            messagebox.showerror("Invalid Input", "Invalid operation. Please enter a valid operation (+, -, *, /).")
+            messagebox.showerror("Invalid Input", "⚠️ Invalid operation. Please enter a valid operation (+, -, *, /).")
             return
 
-        loading_label = tk.Label(self.root, text="Loading")
+        loading_label = tk.Label(self.root, text="Loading", bg="#FDEFEF", fg="#FFACAC")
         loading_label.pack()
         self.animate_loading(loading_label, duration=5)
         messagebox.showinfo("Answer", f"Input 1: {num_1}\nInput 2: {num_2}\nOperation: {operation}\n{result}")

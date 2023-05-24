e56deba529cd2319827b282fceae056f1e8ce416
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-24 09:01:56 +0800

    Input for num 1

--- MAIN.py
+++ MAIN.py
@@ -1,10 +1,15 @@
 import tkinter as tk
+from USERINTERFACE import UserInterface
+ui = UserInterface
+
 root = tk.Tk()
 root.geometry("400x300")
 root.title("My Calculator")
 
 label = tk.Label(root, text="My Calculator", font=("Georgia", 16))
 label.pack(padx=20, pady=10)
+
+ui.UserInput()
 
 
 root.mainloop()
--- USERINTERFACE.py
+++ USERINTERFACE.py
@@ -1,2 +1,14 @@
+import tkinter as tk
+root = tk.Tk()
+class UserInterface:
+    def execute():
+        num_1 = entry1.get()
+
+#Ask the user for input
+label1 = tk.Label(root, text="Input the first number:")
+label1.pack()
+entry1 = tk.Entry(root)
+entry1.pack()
 
 
+root.mainloop()
